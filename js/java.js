52fe3f95ec42f2d22832cbe35c807e99edcb76cf
jhonson2021 <u202219657@upc.edu.pe>
2025-05-15 15:12:26 -0500

    feat: language toggle

--- js/java.js
+++ js/java.js
@@ -1,6 +1,15 @@
 const menuBTN = document.querySelector('.menu-btn');
 const navLinks = document.querySelector('.nav-links');
 
+var check=document.querySelector('.check');
+check.checked = localStorage.getItem("idioma") === "es"; 
+
+check.addEventListener('change', function() {
+    let id = check.checked;
+    localStorage.setItem("idioma", id ? "es" : "en");
+    location.href = id ? "/es/index.html" : "/index.html";
+});
+
 
 menuBTN.addEventListener('click', () => {
     navLinks.classList.toggle('mobile-menu');
